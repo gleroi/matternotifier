c7f57f9f0e781e88a9ae4f728a51414f5c351b20
Guillaume Leroi <guillaume@leroi.re>
2020-07-24 02:03:21 +0200

    mm: after authorize, follow complete

--- matternotifier/src/main.rs
+++ matternotifier/src/main.rs
@@ -1,6 +1,8 @@
 extern crate mm;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let c = mm::Client::new();
-    c.login_with_gitlab("test", "testtest")
+    let c = mm::Client::new("https://192.168.122.76:8065");
+    let token = c.login_with_gitlab("test", "testtest")?;
+    println!("token is {}", token);
+    Ok(())
 }
--- mm/src/lib.rs
+++ mm/src/lib.rs
@@ -20,7 +20,7 @@ pub struct MError {
     str: String,
 }
 
-fn error(str: &str) -> Result<(), Box<dyn Error>> {
+fn error<T>(str: &str) -> Result<T, Box<dyn Error>> {
     Err(Box::new(MError {
         str: str.to_owned(),
     }))
@@ -48,8 +48,19 @@ fn get_input_value(e: &ElementRef, name: &str) -> Result<String, String> {
     Ok(value.unwrap().to_owned())
 }
 
+fn get_header_location(resp: &blocking::Response) -> Result<&str, Box<dyn Error>> {
+    let value = resp
+        .headers()
+        .get(reqwest::header::LOCATION)
+        .ok_or("NO LOCATION");
+    match value {
+        Err(err) => error(err),
+        Ok(header_value) => Ok(header_value.to_str()?),
+    }
+}
+
 impl Client {
-    pub fn new() -> Self {
+    pub fn new(base_url: &str) -> Self {
         let c = blocking::Client::builder()
             .cookie_store(true)
             .danger_accept_invalid_certs(true)
@@ -58,11 +69,16 @@ impl Client {
             .unwrap();
         Client {
             client: c,
-            base_url: "https://192.168.122.76:8065".to_owned(),
+            base_url: base_url.to_owned(),
         }
     }
 
-    pub fn login_with_gitlab(&self, username: &str, password: &str) -> Result<(), Box<dyn Error>> {
+    /// login_with_gitlab returns a mattermost token or else an error
+    pub fn login_with_gitlab(
+        &self,
+        username: &str,
+        password: &str,
+    ) -> Result<String, Box<dyn Error>> {
         // go to mattermost /oauth/gitlab/login, get redirected to gitlab /oauth/authorize
         let url = format!("{}/{}", self.base_url, "oauth/gitlab/login");
         let resp = self.client.get(url.as_str()).send()?;
@@ -73,22 +89,25 @@ impl Client {
                 resp.url()
             ));
         }
-        let redirect_str = resp
-            .headers()
-            .get(reqwest::header::LOCATION)
-            .ok_or("NO LOCATION")?
-            .to_str()?;
-        let authorize_url = reqwest::Url::parse(redirect_str)?;
+        let redirect_str = get_header_location(&resp)?;
+        self.call_gitlab_authorize(redirect_str, username, password)
+    }
+
+    fn call_gitlab_authorize(
+        &self,
+        url: &str,
+        username: &str,
+        password: &str,
+    ) -> Result<String, Box<dyn Error>> {
+        let authorize_url = reqwest::Url::parse(url)?;
         if authorize_url.path() != "/oauth/authorize" {
             return error(&format!(
-                "expected /oauth/authorize, got {}\n  url {}",
-                authorize_url.path(),
-                resp.url()
+                "expected /oauth/authorize, got {}",
+                authorize_url.path()
             ));
         }
 
-        // call gitlab/oauth/authorize
-        let resp = self.client.get(authorize_url.as_str()).send()?;
+        let resp = self.client.get(url).send()?;
         if !resp.status().is_redirection() {
             return error(&format!("expected a redirection, got {}", resp.status()));
         }
@@ -105,29 +124,52 @@ impl Client {
                 .get(login_or_complete_redirection.as_str())
                 .send()?;
             let content = resp.text()?;
-            let doc = Html::parse_document(&content);
-            let form_selector = Selector::parse("#new_user").unwrap();
-            let form_html = doc.select(&form_selector).last().unwrap();
-            let utf8 = get_input_value(&form_html, "utf8")?;
-            let authenticity_token = get_input_value(&form_html, "authenticity_token")?;
-
-            let form = blocking::multipart::Form::new()
-                .text("utf8", utf8)
-                .text("authenticity_token", authenticity_token)
-                .text("user[login]", username.to_owned())
-                .text("user[password]", password.to_owned())
-                .text("user[remember_me]", "1");
+            let resp = self.submit_gitlab_form(
+                login_or_complete_redirection.as_str(),
+                &content,
+                username,
+                password,
+            )?;
+            // now follow to gitlab/authorize and then to complete
+            return self.call_gitlab_authorize(get_header_location(&resp)?, username, password);
+        }
+        if login_or_complete_redirection.path() == "/signup/gitlab/complete" {
+            // complete authentication to get token
             let resp = self
                 .client
-                .post(login_or_complete_redirection)
-                .multipart(form)
+                .get(login_or_complete_redirection.as_str())
                 .send()?;
-            println!("{:?}", resp);
-        // now follow to gitlab/authorize and then to complete
-        } else if login_or_complete_redirection.path() == "/signup/gitlab/complete" {
-            unimplemented!("follow to complete and get token")
+            let header_token = resp.headers().get("token").ok_or("NO TOKEN!!!")?;
+            return Ok(header_token.to_str()?.to_owned());
         }
+        error(&format!(
+            "unknown url while authenticating to gitlab:\n {}",
+            login_or_complete_redirection.path()
+        ))
+    }
 
-        Ok(())
+    fn submit_gitlab_form(
+        &self,
+        url: &str,
+        page: &str,
+        username: &str,
+        password: &str,
+    ) -> Result<blocking::Response, Box<dyn Error>> {
+        let doc = Html::parse_document(page);
+        let form_selector = Selector::parse("#new_user").unwrap();
+        let form_html = doc
+            .select(&form_selector)
+            .last()
+            .ok_or("#new_user not found in form")?;
+        let utf8 = get_input_value(&form_html, "utf8")?;
+        let authenticity_token = get_input_value(&form_html, "authenticity_token")?;
+
+        let form = blocking::multipart::Form::new()
+            .text("utf8", utf8)
+            .text("authenticity_token", authenticity_token)
+            .text("user[login]", username.to_owned())
+            .text("user[password]", password.to_owned())
+            .text("user[remember_me]", "1");
+        Ok(self.client.post(url).multipart(form).send()?)
     }
 }
