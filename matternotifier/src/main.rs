13ea1943760663863d32c086b0e973d79b7c17c2
Guillaume Leroi <guillaume@leroi.re>
2020-07-28 01:49:59 +0200

    fix typo

--- matternotifier/src/main.rs
+++ matternotifier/src/main.rs
@@ -3,8 +3,8 @@ use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = env::var_os("MM_URL").ok_or("Please define env var MM_URL")?;
-    let user = env::var_os("MM_USER").ok_or("Please define env var MM_URL")?;
-    let pass = env::var_os("MM_PASS").ok_or("Please define env var MM_URL")?;
+    let user = env::var_os("MM_USER").ok_or("Please define env var MM_USER")?;
+    let pass = env::var_os("MM_PASS").ok_or("Please define env var MM_PASS")?;
     let mut c = mm::Client::new(url.to_str().unwrap());
     let token = c.login_with_gitlab(user.to_str().unwrap(), pass.to_str().unwrap())?;
     println!("token is {}", token);
