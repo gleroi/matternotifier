3dc173bcd3d022d359e420e8edf67a62e0b19086
Guillaume Leroi <guillaume@leroi.re>
2020-07-27 19:06:01 +0200

    api: users/{user_id}

--- mm/src/lib.rs
+++ mm/src/lib.rs
@@ -1,10 +1,9 @@
-extern crate reqwest;
-extern crate scraper;
-
-use reqwest::blocking;
-use reqwest::Proxy;
 use std::error::Error;
 use std::fmt;
+
+use reqwest::blocking;
+
+use serde::Deserialize;
 
 use scraper::ElementRef;
 use scraper::Html;
@@ -14,6 +13,7 @@ use scraper::Selector;
 pub struct Client {
     client: blocking::Client,
     base_url: String,
+    token: String,
 }
 
 #[derive(Debug, Clone)]
@@ -50,7 +50,7 @@ fn get_input_value(e: &ElementRef, name: &str) -> Result<String, String> {
 }
 
 fn get_header_location(resp: &blocking::Response) -> Result<&str, Box<dyn Error>> {
-        let value = resp
+    let value = resp
         .headers()
         .get(reqwest::header::LOCATION)
         .ok_or(format!("NO LOCATION: \n{:?}", resp));
@@ -65,7 +65,9 @@ impl Client {
         let c = blocking::Client::builder()
             .cookie_store(true)
             .danger_accept_invalid_certs(true)
-            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0")
+            .user_agent(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
+            )
             .redirect(reqwest::redirect::Policy::none())
             .no_proxy()
             .build()
@@ -73,17 +75,25 @@ impl Client {
         Client {
             client: c,
             base_url: base_url.to_owned(),
+            token: "".to_owned(),
         }
+    }
+
+    fn url(&self, path: &str) -> reqwest::Url {
+        reqwest::Url::parse(&self.base_url)
+            .unwrap()
+            .join(path)
+            .unwrap()
     }
 
     /// login_with_gitlab returns a mattermost token or else an error
     pub fn login_with_gitlab(
-        &self,
+        &mut self,
         username: &str,
         password: &str,
     ) -> Result<String, Box<dyn Error>> {
         // go to mattermost /oauth/gitlab/login, get redirected to gitlab /oauth/authorize
-        let url = format!("{}/{}", self.base_url, "oauth/gitlab/login");
+        let url = self.url("oauth/gitlab/login");
         let resp = self.client.get(url.as_str()).send()?;
         if !resp.status().is_redirection() {
             return error(&format!(
@@ -97,17 +107,14 @@ impl Client {
     }
 
     fn call_gitlab_authorize(
-        &self,
+        &mut self,
         url: &str,
         username: &str,
         password: &str,
     ) -> Result<String, Box<dyn Error>> {
         let authorize_url = reqwest::Url::parse(url)?;
         if authorize_url.path() != "/oauth/authorize" {
-            return error(&format!(
-                "expected /oauth/authorize, got {}",
-                authorize_url
-            ));
+            return error(&format!("expected /oauth/authorize, got {}", authorize_url));
         }
 
         let resp = self.client.get(url).send()?;
@@ -122,16 +129,17 @@ impl Client {
                 .client
                 .get(login_or_complete_redirection.as_str())
                 .send()?;
-            let origin = login_or_complete_redirection.origin().unicode_serialization();
+            let origin = login_or_complete_redirection
+                .origin()
+                .unicode_serialization();
             let content = resp.text()?;
-            let resp = self.submit_gitlab_ldap_form(
-                &origin,
-                &content,
-                username,
-                password,
-            )?;
+            let resp = self.submit_gitlab_ldap_form(&origin, &content, username, password)?;
             if !resp.status().is_redirection() {
-                return error(&format!("expected a redirection, got {}\n{:?}", resp.status(), resp.text()?));
+                return error(&format!(
+                    "expected a redirection, got {}\n{:?}",
+                    resp.status(),
+                    resp.text()?
+                ));
             }
             // now follow to gitlab/authorize and then to complete
             return self.call_gitlab_authorize(get_header_location(&resp)?, username, password);
@@ -143,14 +151,15 @@ impl Client {
                 .get(login_or_complete_redirection.as_str())
                 .send()?;
             let header_token = resp.headers().get("token").ok_or("NO TOKEN!!!")?;
-            return Ok(header_token.to_str()?.to_owned());
+            let token = header_token.to_str()?.to_owned();
+            self.token = token.clone();
+            return Ok(token);
         }
         error(&format!(
             "unknown url while authenticating to gitlab:\n {}",
             login_or_complete_redirection.path()
         ))
     }
-
 
     fn submit_gitlab_form(
         &self,
@@ -165,7 +174,10 @@ impl Client {
             .select(&form_selector)
             .last()
             .ok_or("#new_user not found in form")?;
-        let url_path = form_html.value().attr("action").ok_or("form has not action attribute")?;
+        let url_path = form_html
+            .value()
+            .attr("action")
+            .ok_or("form has not action attribute")?;
         let utf8 = get_input_value(&form_html, "utf8")?;
         let authenticity_token = get_input_value(&form_html, "authenticity_token")?;
 
@@ -193,7 +205,10 @@ impl Client {
             .select(&form_selector)
             .last()
             .ok_or("#new_ldap_user not found in form")?;
-        let url_path = form_html.value().attr("action").ok_or("form has not action attribute")?;
+        let url_path = form_html
+            .value()
+            .attr("action")
+            .ok_or("form has not action attribute")?;
         let utf8 = get_input_value(&form_html, "utf8")?;
         let authenticity_token = get_input_value(&form_html, "authenticity_token")?;
 
@@ -209,11 +224,16 @@ impl Client {
         Ok(req.send()?)
     }
 
-    fn get_user(user_id: &str) -> Result<User, Box<dyn Error>> {
-        error("not implemented")
+    pub fn get_user(&self, user_id: &str) -> Result<User, Box<dyn Error>> {
+        let url = self.url(&format!("/api/v4/users/{}", user_id));
+        let req = self.client.get(url);
+        let resp = req.send()?;
+        let user = resp.json::<User>()?;
+        return Ok(user);
     }
 }
 
+#[derive(Default, Debug, Deserialize)]
 pub struct User {
     id: String,
     /// The time in milliseconds a user was created
@@ -222,7 +242,6 @@ pub struct User {
     update_at: i64,
     /// The time in milliseconds a user was deleted
     delete_at: i64,
-
     username: String,
     first_name: String,
     last_name: String,
@@ -236,35 +255,37 @@ pub struct User {
     //props	: object
     last_password_update: i64,
     last_picture_update: i64,
-    failed_attempts: i64,
-    mfa_active: bool,
+    failed_attempts: Option<i64>,
+    mfa_active: Option<bool>,
     timezone: Timezone,
     // ID of accepted terms of service, if any. This field is not present if empty.
-    terms_of_service_id: String,
+    terms_of_service_id: Option<String>,
     /// The time in milliseconds the user accepted the terms of service
-    terms_of_service_create_at: i64,
+    terms_of_service_create_at: Option<i64>,
 }
 
+#[derive(Default, Debug, Deserialize)]
 pub struct UserNotifyProps {
     /// Set to "true" to enable email notifications, "false" to disable. Defaults to "true".
-    email: bool,
+    email: String,
     /// Set to "all" to receive push notifications for all activity, "mention" for mentions and direct messages only, and "none" to disable. Defaults to "mention".
     push: String,
     /// Set to "all" to receive desktop notifications for all activity, "mention" for mentions and direct messages only, and "none" to disable. Defaults to "all".
     desktop: String,
     /// Set to "true" to enable sound on desktop notifications, "false" to disable. Defaults to "true".
-    desktop_sound: bool,
+    desktop_sound: String,
     /// A comma-separated list of words to count as mentions. Defaults to username and @username.
     mention_keys: String,
     /// Set to "true" to enable channel-wide notifications (@channel, @all, etc.), "false" to disable. Defaults to "true".
-    channel: bool,
+    channel: String,
     /// Set to "true" to enable mentions for first name. Defaults to "true" if a first name is set, "false" otherwise.
-    first_name: bool,
+    first_name: String,
 }
 
+#[derive(Default, Debug, Deserialize)]
 pub struct Timezone {
     /// Set to "true" to use the browser/system timezone, "false" to set manually. Defaults to "true".
-    useAutomaticTimezone: bool,
+    useAutomaticTimezone: String,
 
     /// Value when setting manually the timezone, i.e. "Europe/Berlin".
     manualTimezone: String,
