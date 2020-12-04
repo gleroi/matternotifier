009c205081a59fa939413e29f34ccdc560243e20
Guillaume Leroi <guillaume@leroi.re>
2020-12-04 10:01:24 +0100

    rename module gtk to ui

---
+++ rogchat/src/ui.rs
@@ -0,0 +1,40 @@
+
+use gio::prelude::*;
+use gio::ListStore;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button, Grid, Label, ListBox, Notebook, ScrolledWindow};
+
+pub fn test_gtk() {
+    let application =
+        Application::new(Some("re.leroi.rogchat"), Default::default())
+            .expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("rogchat");
+        window.set_default_size(350, 70);
+
+        let notebook = Notebook::new();
+        let g1 = create_grid("Coucou");
+        let g2 = create_grid("Hello");
+        notebook.add(&g1);
+        notebook.set_tab_label_text(&g1, "grille 1");
+        notebook.add(&g2);
+        notebook.set_tab_label_text(&g2, "grille 2");
+        window.add(&notebook);
+        window.show_all();
+    });
+
+    application.run(&[]);
+}
+
+fn create_grid(title: &str) -> gtk::ScrolledWindow {
+    let sw = ScrolledWindow::new(None::<&gtk::Adjustment>, None::<&gtk::Adjustment>);
+    let grid = ListBox::new();
+    for i in 1..5 {
+        let label = Label::new(Some(&format!("label {}", i)));
+        grid.add(&label);    
+    }
+    sw.add(&grid);
+    sw
+}
