1bf93935887b1d9f8fc10567a9522ca9d7c9d4b0
Guillaume Leroi <guillaume@leroi.re>
2020-12-07 17:49:53 +0100

    ui: small cleanup

--- rogchat/src/ui.rs
+++ rogchat/src/ui.rs
@@ -15,12 +15,8 @@ pub fn build() -> Result<Application> {
         window.set_default_size(350, 70);
 
         let notebook = Notebook::new();
-        let g1 = create_grid("Coucou");
-        let g2 = create_grid("Hello");
-        notebook.add(&g1);
-        notebook.set_tab_label_text(&g1, "grille 1");
-        notebook.add(&g2);
-        notebook.set_tab_label_text(&g2, "grille 2");
+        add_chat(&notebook, "Coucou");
+        add_chat(&notebook, "Hello");
         window.add(&notebook);
         window.show_all();
     });
@@ -28,9 +24,10 @@ pub fn build() -> Result<Application> {
     Ok(application)
 }
 
-fn create_grid(title: &str) -> gtk::ScrolledWindow {
+fn add_chat(notebook: &Notebook, title: &str) {
     let v = gtk::TextView::new();
     let window = ScrolledWindow::new(None::<&gtk::Adjustment>, None::<&gtk::Adjustment>);
     window.add(&v);
-    window
+    notebook.add(&window);
+    notebook.set_tab_label_text(&window, title);
 }
