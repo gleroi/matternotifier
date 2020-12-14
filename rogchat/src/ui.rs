4951120ac91a4a828ed2e8409e0cefc7688dfb78
Guillaume Leroi <guillaume@leroi.re>
2020-12-14 17:57:07 +0100

    ui: play with tags

--- rogchat/src/ui.rs
+++ rogchat/src/ui.rs
@@ -1,8 +1,9 @@
 use super::core;
 use glib;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, TextBufferExt, WidgetExt};
-use gtk::{Notebook, ScrolledWindow, TextTagTable};
+use gtk::{Application, ApplicationWindow, TextBuffer, TextBufferExt, WidgetExt};
+use gtk::{Notebook, ScrolledWindow, TextTag, TextTagExt, TextTagTable, TextTagTableExt};
+use pango;
 
 pub fn build(app: &Application, ui_rx: core::Receiver) {
     let window = ApplicationWindow::new(app);
@@ -16,10 +17,10 @@ pub fn build(app: &Application, ui_rx: core::Receiver) {
     ui_rx.attach(None, move |m| {
         match m {
             core::Event::Message(str) => {
-                buffer.insert(&mut buffer.get_end_iter(), &format!("{}\n", str))
+                insert_with_tag(&buffer, "msg", &format!("{}\n", str));
             }
             core::Event::Info(str) => {
-                buffer.insert(&mut buffer.get_end_iter(), &format!("{}\n", str))
+                insert_with_tag(&buffer, "info", &format!("{}\n", str));
             }
         };
         glib::source::Continue(true)
@@ -27,8 +28,27 @@ pub fn build(app: &Application, ui_rx: core::Receiver) {
     window.show_all();
 }
 
+fn insert_with_tag(buffer: &TextBuffer, tag_name: &str, content: &str) {
+    let mut end = buffer.get_end_iter();
+    buffer.insert(&mut end, content);
+    let mut start = end.clone();
+    start.backward_chars(content.len() as i32);
+    buffer.apply_tag_by_name(tag_name, &start, &end)
+}
+
 fn add_chat(notebook: &Notebook, title: &str) -> gtk::TextBuffer {
-    let buffer = gtk::TextBuffer::new(None::<&TextTagTable>);
+    let tags = TextTagTable::new();
+
+    let info_tag = TextTag::new(Some("info"));
+    info_tag.set_property_background(Some("#DDDDDD"));
+    info_tag.set_property_style(pango::Style::Italic);
+    tags.add(&info_tag);
+    
+    let msg_tag = TextTag::new(Some("msg"));
+    msg_tag.set_property_background(Some("#e4eaf5"));
+    tags.add(&msg_tag);
+
+    let buffer = TextBuffer::new(Some(&tags));
     let v = gtk::TextView::with_buffer(&buffer);
     v.set_wrap_mode(gtk::WrapMode::Word);
     v.set_cursor_visible(false);
