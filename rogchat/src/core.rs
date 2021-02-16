078dca427afa40a02286f08d592c4ed3f2b56a96
Guillaume Leroi <guillaume@leroi.re>
2021-02-17 00:36:08 +0100

    dreaming

--- rogchat/src/core.rs
+++ rogchat/src/core.rs
@@ -32,9 +32,29 @@ trait Ui {
     fn create_channel(id: String, name: String, parent: String, users: Vec<User>);
 }
 
-// should go to ui probably
 pub struct Buffer {
     channel: Channel,
     users: Vec<User>,
     messages: Vec<Message>,
 }
+
+// core provides 
+// - buffers: to store messages, from a room/channel/conversation, and a list of participants
+// - plugins: initialize and start plugins
+// - config: manage configuration for itself and plugins
+//
+// plugins provides
+// - protocols: translate a protocol (mattermost, irc,..) to core::Buffer
+// - storage: store/log messages, query history ??
+// - ui enhancement: ???
+// - ...
+//
+// ui provides
+// - a window with
+//   - buffer list: plugin > group > .. > channel (buffer)
+//   - user list: participant of the ui active channel
+//   - buffers "zone":
+//     - buffer display: display messages,
+//     - can be split / tabbed (?)
+//   - input field to send message or command to the plugin managing the current active buffer
+//
